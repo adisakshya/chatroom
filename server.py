9aa7f4899e663979e76460073e56627989bffdc0
Adisakshya Chauhan <45028982+adisakshya@users.noreply.github.com>
2019-01-31 16:51:15 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,31 +1,26 @@
 import socket
-import sys
 
 def main():
 
 	try:
+
 		s = socket.socket()
 		host = socket.gethostname()
 		port = 1234
-		s.bind((host, port))
-		s.listen(5)
-
+		s.connect((host, port))
 		while True:
-			print('Waiting for connecion...')
-			c, addr = s.accept()
-			print('Got connection from IP: ', addr)
-			reply = input('enter reply ')
-			c.send(reply.encode('utf-8'))
-		
-		s.close()	
+			print('From server: ', s.recv(1024).decode('utf-8'))
+			reply = input('send >> ')
+			s.send(reply.encode('utf-8'))
+		s.close()
 
 	except KeyboardInterrupt:
 
-		sys.exit(0)
+		exit(0)
 
 	except Exception as error:
 
-		print('Caught Error: ' + repr(error))	
-
+		print('Caught error: ' + repr(error))
+			
 if __name__ == '__main__':
 	main()
